7a5e44be8d11641700c06d39d05d0df9fce68d29
fiore <cjsdlf44@gmail.com>
2025-12-09 19:13:50 +0900

    초기 구동시 로그에 디버그 모드 명시

--- server.ts
+++
@@ -1,8 +0,0 @@
-import app from "./src/app.ts";
-
-const PORT = 3000;
-const HOST = "0.0.0.0";
-
-app.listen(PORT, HOST, () => {
-    console.log(`Server started on ${HOST}:${PORT}`);
-});
---
+++ src/server.ts
@@ -0,0 +1,12 @@
+import app from "./app.ts";
+
+const PORT = 3000;
+const HOST = "0.0.0.0";
+
+app.listen(PORT, HOST, () => {
+    if (process.env.NODE_ENV !== "production") {
+        console.log(`\x1b[33m[DEBUG MODE]\x1b[0m Server started on ${HOST}:${PORT}`);
+    } else {
+        console.log(`Server started on ${HOST}:${PORT}`);
+    }
+});
